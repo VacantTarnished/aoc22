50ed9dd8f8a4a3e8de63b976811d8f06351992e5
Elias Pühringer <elias.puehringer@outlook.at>
2022-12-12 08:38:37 +0100

    Finished Day 6

---
+++ day_6/src/half_two.rs
@@ -0,0 +1,27 @@
+use std::collections::HashSet;
+
+pub fn half_two() -> usize {
+    let string_input =  std::fs::read_to_string("input/input.txt").expect("Couldn't read Input");
+    let mut four_diff_after: usize = 0;
+    let mut recent_four: Vec<char> = Vec::new();
+
+
+    for char in string_input.chars() {
+        four_diff_after+=1;
+
+        if recent_four.len() < 13 {
+            recent_four.push(char);
+        }
+        else {
+            recent_four.push(char);
+            let diff = recent_four.iter().collect::<HashSet<&char>>().len() == 14;
+            if diff {
+                return four_diff_after;
+            }
+
+            recent_four = recent_four.iter().skip(1).collect::<String>().chars().collect();
+        }
+    }
+
+    panic!("No four different characters in a row found")
+}
