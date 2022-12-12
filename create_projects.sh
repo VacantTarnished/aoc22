65bd2b0eee54bc23badf8a6afb81f300c1c468dc
Elias Pühringer <83457258+VacantTarnished@users.noreply.github.com>
2022-12-12 08:43:31 +0100

    Update create_projects.sh

--- create_projects.sh
+++ create_projects.sh
@@ -5,8 +5,8 @@ project_name="day_"
 
 for ((i=8; i<=$days; i++)); do
     echo "Creating project $project_name$i"
-    #cargo new $project_name$i
-    #mkdir $project_name$i/input
+    cargo new $project_name$i
+    mkdir $project_name$i/input
     touch $project_name$i/input/input.txt
     echo "Done"
 done
